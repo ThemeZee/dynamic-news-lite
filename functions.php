c1d8752e61e92fa0999ddca48653c06956e90b02
Thomas W <netzberufler@googlemail.com>
2015-01-06 15:08:42 +0100

    added unminified javascript

--- functions.php
+++ functions.php
@@ -57,7 +57,7 @@ function dynamicnews_enqueue_html5shiv(){
     
 	/* Embeds HTML5shiv to support HTML5 elements in older IE versions plus CSS Backgrounds */ ?>
 	<!--[if lt IE 9]>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.min.js" type="text/javascript"></script>
 	<![endif]-->
 	
 <?php
