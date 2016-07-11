33525eba38a21e19ce2995b6160de7b72971a033
Thomas W <netzberufler@googlemail.com>
2016-07-11 17:10:43 +0200

    fixed footer navigation icon

--- footer.php
+++ footer.php
@@ -1,24 +1,35 @@
 
 	<?php do_action('dynamicnews_before_footer'); ?>
-	
+
 	<div id="footer-wrap">
-	
+
 		<footer id="footer" class="container clearfix" role="contentinfo">
-			
-			<nav id="footernav" class="clearfix" role="navigation">
-				<?php 
-					// Get Navigation out of Theme Options
-					wp_nav_menu(array('theme_location' => 'footer', 'container' => false, 'menu_id' => 'footernav-menu', 'echo' => true, 'fallback_cb' => '', 'before' => '', 'after' => '', 'link_before' => '', 'link_after' => '', 'depth' => 1));
-				?>
-				<h4 id="footernav-icon"></h4>
-			</nav>
-			
+
 			<div id="footer-text">
 				<?php do_action('dynamicnews_footer_text'); ?>
 			</div>
-			
+
+			<?php // Check if there is a top navigation menu.
+			if ( has_nav_menu( 'footer' ) ) : ?>
+
+				<nav id="footernav" class="clearfix" role="navigation">
+					<h4 id="footernav-icon"></h4>
+					<?php
+						// Get Navigation out of Theme Options
+						wp_nav_menu( array(
+							'theme_location' => 'footer',
+							'container' => false,
+							'menu_id' => 'footernav-menu',
+							'echo' => true, 'fallback_cb' => '',
+							'depth' => 1)
+						);
+					?>
+				</nav>
+
+			<?php endif; ?>
+
 		</footer>
-		
+
 	</div>
 
 </div><!-- end #wrapper -->
